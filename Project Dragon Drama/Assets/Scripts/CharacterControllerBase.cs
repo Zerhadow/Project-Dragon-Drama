d3376ac30b53d285b5af407208574d0948b9ca6a
Skyler <skyguy117@gmail.com>
2023-09-22 15:15:39 -0500

    State Machine Update
    Framework for state machine is complete and just needs linked to dialogue controller. Basic NPC template setup for player interaction.

--- Project Dragon Drama/Assets/Scripts/CharacterControllerBase.cs
+++ Project Dragon Drama/Assets/Scripts/CharacterControllerBase.cs
@@ -9,7 +9,7 @@ public enum PlayerState
 }
 public class CharacterControllerBase : MonoBehaviour
 {
-    [Header("Game Objects")]
+    [Header("Required Objects")]
     [SerializeField] GameObject _playerObject;
     /*Enable once implemented*/
     // [SerializeField] GameObject _cutscenseController;
@@ -21,7 +21,10 @@ public class CharacterControllerBase : MonoBehaviour
     float moveAmountVertical = 0f;
     float moveAmountHorizontal = 0f;
 
+    public GameObject _adjacentNPC = null;
+
     public PlayerState _state;
+    private PlayerState _prevState;
 
     private void Awake()
     {
@@ -31,7 +34,7 @@ public class CharacterControllerBase : MonoBehaviour
 
     private void Update()
     {
-        /* For Test Only */
+        /* --- For Test Only --- */
         if (Input.GetKeyDown(KeyCode.U))
             _state = PlayerState.Menu;
         else if (Input.GetKeyDown(KeyCode.I))
@@ -40,32 +43,92 @@ public class CharacterControllerBase : MonoBehaviour
             _state = PlayerState.NPCTalk;
         else if (Input.GetKeyDown(KeyCode.P))
             _state = PlayerState.Dialogue;
+        /* --- For Test Only --- */
 
         switch (_state)
         {
             case PlayerState.Menu: //Game Paused
-                //Return to game or return to main menu options
-                stopMoving();
-                _playerObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red); //for test visual
-                Debug.Log("Menu State");
+                {
+                    //Return to game or return to main menu options
+                    stopMoving();
+
+                    if (Input.GetKeyDown(KeyCode.Escape)) //Return to previous state
+                    {
+                        _state = _prevState;
+                    }
+
+                    /*Debug for state change*/
+                    DebugColorUpdate(_playerObject, Color.red);
+                    Debug.Log("Menu State");
+                }
                 break;
-            case PlayerState.Moving:
-                MovementInput();
-                //Go to NPCTalk, Dialogue, Menu
-                _playerObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue); //for test visual
-                Debug.Log("Moving State");
+            case PlayerState.Moving: //Player walking around
+                {
+                    //Go to NPCTalk, Dialogue, Menu
+                    MovementInput();
+
+                    if (Input.GetKeyDown(KeyCode.Escape)) //GoTo Pause
+                    {
+                        _prevState = _state;
+                        _state = PlayerState.Menu;
+                    }
+                    else if ((Input.GetKeyDown(KeyCode.E)) && (_adjacentNPC != null)) //Talk to NPC, GoTo NPCTalk
+                    {
+                        // Open dialogue popup tied to adjacentNPC
+
+                        /*For Testing. Disable once implemented*/
+                        DebugColorUpdate(_adjacentNPC, Color.green);
+                        Debug.Log("Player talking to " + _adjacentNPC.name);
+
+                        _state = PlayerState.NPCTalk;
+                    }
+
+                    /*Debug for state change*/
+                    DebugColorUpdate(_playerObject, Color.blue);
+                    Debug.Log("Moving State");
+                }
                 break;
-            case PlayerState.NPCTalk:
-                //No Movement, Click to progress or dismiss. Dismiss goes back to Moving
-                stopMoving();
-                _playerObject.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow); //for test visual
-                Debug.Log("NPCTalk State");
+            case PlayerState.NPCTalk: //NPC popup dialogue
+                {
+                    //No Movement, Click to progress or dismiss. Dismiss goes back to Moving
+                    stopMoving();
+
+                    if(Input.GetKeyDown(KeyCode.Escape)) //GoTo Pause
+                    {
+                        _prevState = _state;
+                        _state = PlayerState.Menu;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.E)) //Dismiss popup text, GoTo Moving
+                    {
+                        // Close dialogue popup
+
+                        /*For Testing. Disable once implemented*/
+                        DebugColorUpdate(_adjacentNPC, Color.magenta);
+                        Debug.Log(_adjacentNPC.name + " has stopped talking");
+
+                        _state = PlayerState.Moving;
+                    }
+
+                    /*Debug for state change*/
+                    DebugColorUpdate(_playerObject, Color.yellow);
+                    Debug.Log("NPCTalk State");
+                }
                 break;
-            case PlayerState.Dialogue:
-                //Hand off to DialogueController for cutscene. No Movement, Click to progress (speech choice within dialogue controller?). Exits to Moving.
-                stopMoving();
-                _playerObject.GetComponent<Renderer>().material.SetColor("_Color", Color.green); //for test visual
-                Debug.Log("Dialogue State");
+            case PlayerState.Dialogue: //Visual novel cutscene dialoue
+                {
+                    //Hand off to DialogueController for cutscene. No Movement, Click to progress (speech choice within dialogue controller?). Exits to Moving.
+                    stopMoving();
+
+                    if (Input.GetKeyDown(KeyCode.Escape)) //GoTo Pause
+                    {
+                        _prevState = _state;
+                        _state = PlayerState.Menu;
+                    }
+
+                    /*Debug for state change*/
+                    DebugColorUpdate(_playerObject, Color.green);
+                    Debug.Log("Dialogue State");
+                }
                 break;
             default:
                 break;
@@ -77,21 +140,35 @@ public class CharacterControllerBase : MonoBehaviour
         MovePlayer();
     }
 
+    //Translate input for movement (must be separate from Move method due to FixedUpdate)
     private void MovementInput()
     {
         moveAmountVertical = Input.GetAxisRaw("Vertical") * _moveSpeed;
         moveAmountHorizontal = Input.GetAxisRaw("Horizontal") * _moveSpeed;
     }
 
+    //Moves player based on player input (must be in FixedUpdate therefore is always called)
+    private void MovePlayer()
+    { 
+        _player.velocity = new Vector3(moveAmountVertical, 0, -moveAmountHorizontal);
+    }
+
+    //Halts player movement
     private void stopMoving()
     {
         moveAmountVertical = 0f;
         moveAmountHorizontal = 0f;
     }
 
-    //Moves player based on player input
-    private void MovePlayer()
-    { 
-        _player.velocity = new Vector3(moveAmountVertical, 0, -moveAmountHorizontal);
+    //Setter for NPCController
+    public void setAdjacentNPC(GameObject adjacent)
+    {
+        _adjacentNPC = adjacent;
+    }
+
+    //Visuals for testing
+    private void DebugColorUpdate(GameObject gameObject, Color color)
+    {
+        gameObject.GetComponent<Renderer>().material.SetColor("_Color", color);
     }
 }
---
+++ Project Dragon Drama/Assets/Scripts/NPCControllerBase.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCControllerBase : MonoBehaviour
+{
+    [Header ("Required Objects")]
+    [SerializeField] GameObject _npc;
+    [SerializeField] GameObject _characterController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") //Player has entered talk range
+        {
+            _characterController.GetComponent<CharacterControllerBase>().setAdjacentNPC(_npc); //Set this NPC as adjacentNPC on Character Controller
+
+            /*For Debug Tesing*/
+            _npc.GetComponent<Renderer>().material.SetColor("_Color", Color.magenta);
+            Debug.Log("Player in talking range of " + _npc.name);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player") //Player has exited talk range
+        {
+            _characterController.GetComponent<CharacterControllerBase>().setAdjacentNPC(null); //Set adjacentNPC as null on CharacterController
+
+            /*For Debug Tesing*/
+            _npc.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+            Debug.Log("Player has left talking range of " + _npc.name);
+        }
+    }
+}
