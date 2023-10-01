c3798db07be22098e8aacf967edcfc28a7e32aeb
Zachary Morris <119977390+ZachMorrisHere@users.noreply.github.com>
2023-09-30 20:20:05 -0500

    HHEHEHEHEE

---
+++ Project Dragon Drama/Assets/Scripts/TransistionController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransistionController : MonoBehaviour
+{
+    public Animator transistion;
+
+    public float transistionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelindex)
+    {
+        transistion.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transistionTime);
+
+
+        SceneManager.LoadScene(levelindex);
+    }
+}
