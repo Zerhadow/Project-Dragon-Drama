6d50a0e5c445b9fcbfc82a8aa30af13691a9dce6
Julian <jrg180004@utdallas.edu>
2024-04-26 16:58:27 -0500

    started stats

---
+++ Project Dragon Drama/Assets/Scripts/Controllers/StatController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatController : MonoBehaviour
+{
+    [Header("Player Stats")]
+    public int nerd, persuasion, manipulation, deception;
+    
+    [Header("Player Stats with Ppl")]
+    public int sam, persia, jassica, natalie, ken;
+
+    public void ApplyModifier(string str) {
+
+    }
+}
