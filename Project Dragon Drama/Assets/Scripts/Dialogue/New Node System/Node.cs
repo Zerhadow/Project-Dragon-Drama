b5971f90b1bd44bb5a268ab4fdea17835c6f6c00
Julian RaShaun Gay <jrg180004@utdallas.edu>
2024-07-27 16:38:10 -0400

    forgot to remove node content class

--- Project Dragon Drama/Assets/Scripts/Dialogue/New Node System/Node.cs
+++ Project Dragon Drama/Assets/Scripts/Dialogue/New Node System/Node.cs
@@ -8,18 +8,6 @@ public enum NodeType {
     AutoBranch
 }
 
-public class NodeContent {
-    public string speakerName;
-    public string bodyText;
-    public Emotion emotion;
-    public string opt1,opt2,opt3;
-    public NodeList option1NodeList;
-    public NodeList option2NodeList;
-    public NodeList option3NodeList;
-    public NodeList nodeList1;
-    public NodeList nodeList2;
-}
-
 [CreateAssetMenu(menuName = "Dialogue System/Node")]
 public abstract class Node : ScriptableObject
 {
